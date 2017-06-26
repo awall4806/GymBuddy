717dd02643c06b297b10e15ef77ea8aee2c00442
Alex <wallta@appstate.edu>
2017-06-26 16:23:10 -0400

    Setting Up Content in XMLs, Created Spinner for Day List Fragment

--- app/src/main/java/wallta/gymbuddy/DayListFragment.java
+++ app/src/main/java/wallta/gymbuddy/DayListFragment.java
@@ -1,10 +1,54 @@
 package wallta.gymbuddy;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by wallta on 6/26/2017.
  */
 
 public class DayListFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_day_list, container, false);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.day_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.day_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    private void updateUI() {
+        // TODO:
+    }
 }
